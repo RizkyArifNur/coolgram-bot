ad1efd9ab2ad2c0d582980a41f8cab8f0e56398b
RizkyArifNur <rizkyarifnur@gmail.com>
2019-10-29 06:41:55 +0700

    fix(typings): add typing support

---
+++ src/typings/index.ts
@@ -0,0 +1,18 @@
+export interface IMessage {
+  firstName: string
+  lastName: string
+  message: string
+  isQna: boolean
+}
+
+export type KulgramState = 'STOPED' | 'STARTED' | 'START-QNA'
+
+export interface IState {
+  chatId: number
+  state: KulgramState
+}
+
+export interface IRecordedMessages {
+  chatId: number
+  data: IMessage[]
+}
---
+++ src/typings/node.d.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    STATE_PATH?: string
+    BOT_TOKEN?: string
+    MESSAGE_RECORDED_PATH?: string
+  }
+}
